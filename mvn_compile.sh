4d58290dff3c9601d60c4df28d94b5f6a23cf871
fuyiqi <864659174@qq.com>
2022-07-11 21:15:47 +0800

    Update mvn_compile.sh

--- mvn_compile.sh
+++ mvn_compile.sh
@@ -1,2 +1,4 @@
 mvn clean install -T 2C -Dfast -Dmaven.compile.fork=true -DskipTests -Dscala-2.11 -Drat.skip=true -Dmaven.javadoc.skip=true -Dcheckstyle.skip=true -Dskip.npm
 # mvn dependency:get -DremoteRepositories=http://repo1.maven.org/maven2/ -DgroupId=org.apache.maven.plugins -DartifactId=maven-gpg-plugin -Dversion=1.4
+# mvn install -Dfast -Dmaven.compile.fork=true -DskipTests -Dmaven.test.skip=true
+
